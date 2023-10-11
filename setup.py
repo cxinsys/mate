0dd0eeb8843faeb7a6b5e46994972ebb3b517d53
rbsung <rakbin007@naver.com>
2023-10-11 22:49:43 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='mate',
-      version='0.0.1',
+      version='0.0.2',
       description='MATE',
       url='https://github.com/cxinsys/mate',
       author='Complex Intelligent Systems Laboratory (CISLAB)',
